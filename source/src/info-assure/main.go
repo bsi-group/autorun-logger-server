6f1d8be684ec3bff3c63fd3b989af88e7ff476ac
Mark Woan <markwoan@gmail.com>
2016-09-15 08:49:22 +0100

    Corrected version number

--- source/src/info-assure/main.go
+++ source/src/info-assure/main.go
@@ -20,7 +20,7 @@ import (
 
 const APP_TITLE string = "AutoRun Logger Server"
 const APP_NAME string = "arl-server"
-const APP_VERSION string = "0.0.2"
+const APP_VERSION string = "1.0.1"
 
 // ##### Variables ###########################################################
 
